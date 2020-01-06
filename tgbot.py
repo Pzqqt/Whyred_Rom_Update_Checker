fdbf34c78b79f5af91ad939839a38092a35551cd
Pzqqt <821026875@qq.com>
2020-01-06 10:17:14 +0800

    Update tgbot.py - Try again if send message fails - Log detailed exceptions

--- tgbot.py
+++ tgbot.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
+import traceback
+
 import telebot
 
 from config import TG_TOKEN, TG_SENDTO, _PROXIES_DIC
 from logger import write_log_warning
 
-bot = telebot.TeleBot(TG_TOKEN)
+BOT = telebot.TeleBot(TG_TOKEN)
 telebot.apihelper.proxy = _PROXIES_DIC
 
 def send_message(text, user=TG_SENDTO):
     try:
-        bot.send_message(user, text)
-    except Exception as error:
-        print("!", error)
-        print("! Error: failed to post message to Telegram!")
-        write_log_warning("Failed to post message to Telegram!")
+        BOT.send_message(user, text)
+    except:
+        # try again
+        try:
+            BOT.send_message(user, text)
+        except:
+            traceback_string = traceback.format_exc()
+            print(traceback_string)
+            print("! Error: failed to post message to Telegram!")
+            write_log_warning(*traceback_string.splitlines())
+            write_log_warning("Failed to post message to Telegram!")
