64b3887d97fa4f6d7eef97aff56719372ec93ab2
Pzqqt <821026875@qq.com>
2020-01-23 21:34:51 +0800

    Update check_list
    - Remove "New rom release by SkylarkAurora" (developer requirement)

--- check_list.py
+++ check_list.py
@@ -400,10 +400,6 @@ class Revolution(SfCheck):
     def filter_rule(self, string):
         return "RedmiNote5" in string
 
-class SkylarkAuroraProject(SfProjectCheck):
-    project_name = "skylarkaurora"
-    developer = "SkylarkAurora"
-
 class SreekFreaksProject(SfProjectCheck):
     project_name = "sreekfreaks-unofficial-builds"
     developer = "SreekFreaks"
@@ -507,7 +503,6 @@ CHECK_LIST = (
     Rebellion,
     ResurrectionRemix,
     Revolution,
-    SkylarkAuroraProject,
     SreekFreaksProject,
     SrfariasProject,
     Stag,
