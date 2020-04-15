d2a32a07368faa5e0ccc88fb9384f6f8d6a0f362
Pzqqt <821026875@qq.com>
2020-04-15 08:42:42 +0800

    tgbot.py: Fxxk GFW!

--- tgbot.py
+++ tgbot.py
@@ -3,6 +3,7 @@
 
 import traceback
 
+import requests
 import telebot
 
 from config import TG_TOKEN, TG_SENDTO, TIMEOUT, _PROXIES_DIC
@@ -12,15 +13,17 @@ BOT = telebot.TeleBot(TG_TOKEN)
 telebot.apihelper.proxy = _PROXIES_DIC
 
 def send_message(text, user=TG_SENDTO):
-    try:
-        BOT.send_message(user, text, parse_mode="Markdown", timeout=TIMEOUT)
-    except:
-        # try again
+    for _ in range(10):
         try:
             BOT.send_message(user, text, parse_mode="Markdown", timeout=TIMEOUT)
+        except requests.exceptions.SSLError:
+            pass
         except:
             traceback_string = traceback.format_exc()
-            print(traceback_string)
-            print("! Error: failed to post message to Telegram!")
+            print("\n%s\n! Error: failed to post message to Telegram!" % traceback_string)
             write_log_warning(*traceback_string.splitlines())
             write_log_warning("Failed to post message to Telegram!")
+        else:
+            return
+    print("! Fuck GFW!")
+    write_log_warning("Fuck GFW!")
