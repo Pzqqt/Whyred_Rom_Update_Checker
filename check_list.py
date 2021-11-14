598e39248cba6a22af47b93d5614b10cc382ac17
Pzqqt <821026875@qq.com>
2021-11-14 19:43:54 +0800

    check_list: Undo Skip "Paranoid Android Official"
    Now server is fine.
    
    This reverts commit 969bdbbffe5a43dc9f46ac8de8ef84ab22ff7b65.

--- check_list.py
+++ check_list.py
@@ -315,7 +315,6 @@ class AosipDfGapps(H5aiCheck):
 class Aospa(CheckUpdate):
 
     fullname = "Paranoid Android Official"
-    _skip = True
 
     def do_check(self):
         req_text = self.request_url("https://api.aospa.co/updates/whyred")
