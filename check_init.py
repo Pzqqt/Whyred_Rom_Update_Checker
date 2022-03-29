dbfdb02283c871092d139fa598a439c2af6fcf78
Pzqqt <821026875@qq.com>
2022-03-30 00:55:01 +0800

    check_init: Update Chrome user-agent

--- check_init.py
+++ check_init.py
@@ -22,7 +22,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 CHROME_UA = (
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-    "(KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
+    "(KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36"
 )
 
 _KEY_TO_PRINT = {
