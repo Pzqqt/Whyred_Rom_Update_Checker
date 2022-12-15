751a73bffcd193bf9979977d095730131b63b4d0
Pzqqt <821026875@qq.com>
2022-12-15 17:40:49 +0800

    main.py: Fix some type annotations

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from requests import exceptions
 from config import (
     ENABLE_SENDMESSAGE, LOOP_CHECK_INTERVAL, ENABLE_MULTI_THREAD, MAX_THREADS_NUM, LESS_LOG, ENABLE_LOGGER
 )
-from check_init import PAGE_CACHE, CheckUpdate
+from check_init import PAGE_CACHE
 from check_list import CHECK_LIST
 from database import DatabaseSession, Saved
 from logger import write_log_info, write_log_warning, print_and_log, LOGGER
@@ -27,7 +27,7 @@ FORCE_UPDATE = False
 
 _THREADING_LOCK: Final = threading.Lock()
 
-def database_cleanup() -> NoReturn:
+def database_cleanup() -> set[str]:
     """
     将数据库中存在于数据库但不存在于CHECK_LIST的项目删除掉
     :return: 被删除的项目名字的集合
@@ -59,7 +59,7 @@ def _get_time_str(time_num: Optional[float] = None, offset: int = 0) -> str:
         time_num = time.time()
     return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time_num+offset))
 
-def check_one(cls: typing.Union[CheckUpdate, str], disable_pagecache: bool = False) -> bool:
+def check_one(cls: typing.Union[type, str], disable_pagecache: bool = False) -> bool:
     if isinstance(cls, str):
         cls_str = cls
         cls = {cls_.__name__: cls_ for cls_ in CHECK_LIST}.get(cls_str)
@@ -111,7 +111,7 @@ def check_one(cls: typing.Union[CheckUpdate, str], disable_pagecache: bool = Fal
                 write_log_info("%s no update" % cls_obj.fullname)
         return True
 
-def single_thread_check(check_list: typing.Sequence[CheckUpdate]) -> (list, bool):
+def single_thread_check(check_list: typing.Sequence[type]) -> (list, bool):
     # 单线程模式下连续检查失败5项则判定为网络异常, 并提前终止
     req_failed_flag = 0
     check_failed_list = []
@@ -128,7 +128,7 @@ def single_thread_check(check_list: typing.Sequence[CheckUpdate]) -> (list, bool
         _sleep(2)
     return check_failed_list, is_network_error
 
-def multi_thread_check(check_list: typing.Sequence[CheckUpdate]) -> (list, bool):
+def multi_thread_check(check_list: typing.Sequence[type]) -> (list, bool):
     # 多线程模式下累计检查失败10项则判定为网络异常, 并在之后往线程池提交的任务中不再进行检查操作而是直接返回
     check_failed_list = []
     is_network_error = False
