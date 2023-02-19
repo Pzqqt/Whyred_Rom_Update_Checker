8531ffd17a362b05e9f3b28946a53e8747528062
Pzqqt <821026875@qq.com>
2023-02-19 16:44:26 +0800

    check_init.py: Add missing wraps for decorators I wrote

--- check_init.py
+++ check_init.py
@@ -9,7 +9,7 @@ import urllib3
 from typing import Union, NoReturn, Final
 from collections import OrderedDict
 from urllib.parse import unquote, urlencode
-from functools import lru_cache
+from functools import lru_cache, wraps
 
 import requests
 from bs4 import BeautifulSoup
@@ -89,6 +89,7 @@ class CheckUpdate:
         self.send_message = self.__hook_is_checked(self.send_message)
 
     def __hook_do_check(self, method: typing.Callable) -> typing.Callable:
+        @wraps(method)
         def hook(*args, **kwargs):
             method(*args, **kwargs)
             # 如果上一行语句抛出了异常, 将不会执行下面这行语句
@@ -97,6 +98,7 @@ class CheckUpdate:
         return hook
 
     def __hook_is_checked(self, method: typing.Callable) -> typing.Callable:
+        @wraps(method)
         def hook(*args, **kwargs):
             assert self.__is_checked, "Please execute the 'do_check' method first."
             return method(*args, **kwargs)
